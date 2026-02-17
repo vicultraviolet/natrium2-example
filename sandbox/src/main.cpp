0d67011dfd0d494ae26a73dacd2f8832dff0bbc5
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2026-02-17 11:31:15 -0300

    Added natrium initialization
    Added natrium initialization
    Updated `natrium2`

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,12 +1,16 @@
 #include "Pch.hpp"
 #include "Natrium2/Main.hpp"
 
-#include "Natrium2/Core/Logger.hpp"
+#include <Natrium2/Core/Logger.hpp>
+#include <Natrium2/Core/Context.hpp>
 
 Na2::Logger<> logger{ "Sandbox" };
 
 int main(int argc, char* argv[])
 {
+	Na2::Context context(ContextInitInfo{});
+	context.bind();
+
 	logger.printf(Na2::Notice, "Hello, {}!", "world");
 
 	return 0;
